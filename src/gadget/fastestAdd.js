b2754b98061231f495cb51ab131cdd195437dab9
heming <qiaoheming0620@qq.com>
2023-02-08 10:26:16 +0800

    feat: update `fastestAdd.js`

--- src/gadget/fastestAdd.js
+++ src/gadget/fastestAdd.js
@@ -27,45 +27,55 @@ function asyncAdd(a, b, cb) {
  */
 
 function add(a, b) {
-    return new Promise(resolve => {
-        setTimeout(() => resolve(a + b), 200);
+    return new Promise((resolve) => {
+        setTimeout(resolve, 1000, a + b);
     });
 }
 
-function fastestAdd(nums) {
+function sum(nums) {
     return new Promise((resolve, reject) => {
-        parallelAdd();
+        run();
 
-        function parallelAdd() {
-            if (nums.length === 1 && nums[0] !== null) return resolve(nums[0]);
-            while (nums.filter(item => item !== null).length >= 2) doAdd();
-        }
-
-        function doAdd() {
-            if (nums.length === 1 && nums[0] !== null) return resolve(nums[0]);
-            if (nums.filter(item => item !== null).length <= 1) return;
-            let a, b;
-            for (let i = 0; i < nums.length; i++) {
-                if (a === undefined && nums[i] !== null) {
-                    a = nums.splice(i--, 1)[0];
-                } else if (b === undefined && nums[i] !== null) {
-                    b = nums.splice(i, 1)[0];
-                    break;
-                }
-            }
-            nums.push(null); // 使用 null 来进行占位
-            add(a, b).then(res => {
-                for (let i = 0; i < nums.length; i++) {
-                    if (nums[i] === null) {
-                        nums[i] = res;
-                        break;
+        function run() {
+            if (nums.filter(i => i !== null).length > 1) {
+                let a, b;
+                // 能进入这里，上面的if判断确保数组中存在大于等于2个的非null数字，不需要担心findIndex返回-1
+                a = nums.splice(nums.findIndex(i => i !== null), 1)[0];
+                b = nums.splice(nums.findIndex(i => i !== null), 1)[0];
+                nums.push(null);
+                add(a, b).then(res => {
+                    for (let i = 0; i < nums.length; i++) {
+                        if (nums[i] === null) {
+                            nums[i] = res;
+                            break;
+                        }
                     }
-                }
-                parallelAdd();
-            }, err => {
-                reject(err);
-            });
+                    run();
+                }, err => {
+                    reject(err);
+                });
+                // 对 a b 执行完加法后，继续调用run
+                run();
+            } else if (nums.length === 1 && nums[0] !== null) {
+                resolve(nums[0]);
+            }
         }
+    });
+}
+
+console.time("start");
+// expect execute time: 3s
+sum([1, 2, 3, 4, 5, 6, 7]).then(res => {
+    console.log(res)
+    console.timeEnd("start");
+});
+
+
+/*
+
+function add(a, b) {
+    return new Promise(resolve => {
+        setTimeout(() => resolve(a + b), 200);
     });
 }
 
@@ -107,3 +117,5 @@ const nums = [2, 1, 4, 7];
     console.timeEnd("add");
     console.log(res);
 })();
+
+ */
